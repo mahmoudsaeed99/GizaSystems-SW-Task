f9a2db95fe942de05a79b4a02b8f64a60230d4e7
MahmoudSaeed <36303424+mahmoudsaeed99@users.noreply.github.com>
2023-09-03 17:00:46 +0300

    Update SeasonalComponent.py

--- Components/SeasonalComponent.py
+++ Components/SeasonalComponent.py
@@ -8,31 +8,41 @@ from Components.AdditionalComponent import *
 
 class SeasonalComponent(AdditionalComponent):
     
-   def addComponent(self, data, seasonality, season_type , type_ = 'daily'):
-       """
-       Add seasonality component to the time series data.
+   def add_weekly(data, seasonality, season_type):
+    """
+    Add weekly seasonality component to the time series data.
 
-       Parameters:
-           data (DatetimeIndex): The time index for the data.
-           seasonality (str): The type of seasonality ('Long', 'Short', or 'Intermediate').
+    Parameters:
+        data (DatetimeIndex): The time index for the data.
+        seasonality (str): The type of seasonality ('Long', 'Short', or 'Intermediate').
 
-       Returns:
-           numpy.ndarray: The seasonal component of the time series.
-       """
-       # check add type like(`weekly` or `daily`)
-       
-       if type_.lower() == 'daily':
-           season = 24
-           season_data = data.hour
-           
-       elif type_.lower() == 'weekly':
-           season = 7
-           season_data = data.dayofweek
-           
-       if seasonality == "exist":  # Weekly Seasonality
-           seasonal_component = np.sin(2 * np.pi * season_data / season)
-           seasonal_component += 1 if season_type == 'multiplicative' else 0
-       else:
-           seasonal_component = np.zeros(len(data)) if season_type == 'additive' else np.ones(len(data))
-       return pd.Series(seasonal_component)
+    Returns:
+        numpy.ndarray: The seasonal component of the time series.
+    """
+    if seasonality == "exist":  # Weekly Seasonality
+        seasonal_component = np.sin(2 * np.pi * data.dayofweek / 7)
+        seasonal_component += 1 if season_type == 'multiplicative' else 0
+    else:
+        seasonal_component = np.zeros(len(data)) if season_type == 'additive' else np.ones(len(data))
+    return pd.Series(seasonal_component)
+
+
+def add_daily(data, seasonality, season_type):
+    """
+    Add seasonality component to the time series data.
+
+    Parameters:
+        data (DatetimeIndex): The time index for the data.
+        seasonality (str): The type of seasonality ('Long', 'Short', or 'Intermediate').
+
+    Returns:
+        numpy.ndarray: The seasonal component of the time series.
+    """
+    if seasonality == "exist":  # Daily Seasonality
+        seasonal_component = np.sin(2 * np.pi * data.hour / 24)
+        seasonal_component += 1 if season_type == 'multiplicative' else 0
+    else:
+        seasonal_component = np.zeros(len(data)) if season_type == 'additive' else np.ones(len(data))
+    return pd.Series(seasonal_component)
+
 
