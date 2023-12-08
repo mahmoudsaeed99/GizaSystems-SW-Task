0fbadfd35bd1254715cf1b244a1acaf046a1c70f
Mahmoud Saeed Mesmeh <mahmoud.mesmeh@gizasystems.local>
2023-12-08 13:29:20 +0200

    update automl

--- AutoMl/main.py
+++ AutoMl/main.py
@@ -14,19 +14,32 @@ app = Flask(__name__)
 import pickle
 # loaded_model = pickle.load(open('knnpickle_file', 'rb'))
 # result = loaded_model.predict(X_test)
+df_train = pd.read_csv("Train_Split.csv")
+df_train.drop(['variance'], axis=1, inplace=True)
+
+# Create and fit KNeighborsClassifier
 loaded_calibrated_model = KNeighborsClassifier(n_neighbors=3, p=1, weights='distance')
+loaded_calibrated_model.fit(df_train.drop(['y'], axis=1), df_train['y'])
+
 @app.route("/calibrated_AutoML/predict" , methods = ['POST'])
 def predict():
-    df = pd.read_csv("Train_Split.csv")
-    df.drop(['variance'] , axis = 1 , inplace=True)
-    loaded_calibrated_model.fit(df.drop(['y'],axis = 1) , df['y'])
+
     # {“variance”:z1, “skewness”:z2, “curtosis”:z3, “entropy”:z4}
     data = request.get_json()
     df = pd.DataFrame(data, index=[0])
     if "variance" in df.columns:
         df.drop(['variance'] , axis = 1 , inplace = True)
-    pred = loaded_calibrated_model.predict(df)[0]
-    result = {"output": int(pred)}
+    # Get the indices of the k-nearest neighbors
+    k_neighbors_indices = loaded_calibrated_model.kneighbors(df, return_distance=False).flatten()
+
+    # Get the labels of the k-nearest neighbors
+    k_neighbors_labels = df_train.iloc[k_neighbors_indices]['y']
+
+    # Calculate the predicted probability for the positive class
+    predicted_probability_positive_class = k_neighbors_labels.mean()
+    print(predicted_probability_positive_class)
+    prob_positive_class = loaded_calibrated_model.predict_proba(df)
+    result = {"output": int(prob_positive_class[:, 1].item())}
     return jsonify(result)
 
 if __name__ == "__main__":
